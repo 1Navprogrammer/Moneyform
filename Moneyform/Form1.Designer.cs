0a26df1341736ccf44df26ace9a408343f815c9d
Navpreet Kaur <NK028@atcltd.atc.org.nz>
2020-06-12 14:50:59 +1200

    edit

---
+++ Moneyform/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Moneyform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TotalmoneyTextbox = new System.Windows.Forms.TextBox();
+            this.Gobutton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.v = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.HundredthTextbox = new System.Windows.Forms.TextBox();
+            this.TenthTextbox = new System.Windows.Forms.TextBox();
+            this.FifthTextbox = new System.Windows.Forms.TextBox();
+            this.TwentythTextbox = new System.Windows.Forms.TextBox();
+            this.FiftythTextbox = new System.Windows.Forms.TextBox();
+            this.SecondTextbox = new System.Windows.Forms.TextBox();
+            this.FirstTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(336, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Money";
+            // 
+            // TotalmoneyTextbox
+            // 
+            this.TotalmoneyTextbox.Location = new System.Drawing.Point(398, 38);
+            this.TotalmoneyTextbox.Name = "TotalmoneyTextbox";
+            this.TotalmoneyTextbox.Size = new System.Drawing.Size(100, 26);
+            this.TotalmoneyTextbox.TabIndex = 1;
+            this.TotalmoneyTextbox.Text = "596";
+            // 
+            // Gobutton
+            // 
+            this.Gobutton.Location = new System.Drawing.Point(356, 83);
+            this.Gobutton.Name = "Gobutton";
+            this.Gobutton.Size = new System.Drawing.Size(85, 32);
+            this.Gobutton.TabIndex = 2;
+            this.Gobutton.Text = "Go";
+            this.Gobutton.UseVisualStyleBackColor = true;
+            this.Gobutton.Click += new System.EventHandler(this.Gobutton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(372, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(452, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(293, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "20";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(205, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "50";
+            // 
+            // v
+            // 
+            this.v.AutoSize = true;
+            this.v.Location = new System.Drawing.Point(125, 215);
+            this.v.Name = "v";
+            this.v.Size = new System.Drawing.Size(36, 20);
+            this.v.TabIndex = 7;
+            this.v.Text = "100";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(621, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(531, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "2";
+            // 
+            // HundredthTextbox
+            // 
+            this.HundredthTextbox.Location = new System.Drawing.Point(110, 251);
+            this.HundredthTextbox.Name = "HundredthTextbox";
+            this.HundredthTextbox.Size = new System.Drawing.Size(65, 26);
+            this.HundredthTextbox.TabIndex = 10;
+            // 
+            // TenthTextbox
+            // 
+            this.TenthTextbox.Location = new System.Drawing.Point(362, 251);
+            this.TenthTextbox.Name = "TenthTextbox";
+            this.TenthTextbox.Size = new System.Drawing.Size(65, 26);
+            this.TenthTextbox.TabIndex = 12;
+            // 
+            // FifthTextbox
+            // 
+            this.FifthTextbox.Location = new System.Drawing.Point(433, 251);
+            this.FifthTextbox.Name = "FifthTextbox";
+            this.FifthTextbox.Size = new System.Drawing.Size(65, 26);
+            this.FifthTextbox.TabIndex = 13;
+            // 
+            // TwentythTextbox
+            // 
+            this.TwentythTextbox.Location = new System.Drawing.Point(281, 251);
+            this.TwentythTextbox.Name = "TwentythTextbox";
+            this.TwentythTextbox.Size = new System.Drawing.Size(65, 26);
+            this.TwentythTextbox.TabIndex = 14;
+            this.TwentythTextbox.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // FiftythTextbox
+            // 
+            this.FiftythTextbox.Location = new System.Drawing.Point(196, 251);
+            this.FiftythTextbox.Name = "FiftythTextbox";
+            this.FiftythTextbox.Size = new System.Drawing.Size(65, 26);
+            this.FiftythTextbox.TabIndex = 15;
+            this.FiftythTextbox.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // SecondTextbox
+            // 
+            this.SecondTextbox.Location = new System.Drawing.Point(511, 251);
+            this.SecondTextbox.Name = "SecondTextbox";
+            this.SecondTextbox.Size = new System.Drawing.Size(65, 26);
+            this.SecondTextbox.TabIndex = 16;
+            this.SecondTextbox.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // FirstTextbox
+            // 
+            this.FirstTextbox.Location = new System.Drawing.Point(600, 251);
+            this.FirstTextbox.Name = "FirstTextbox";
+            this.FirstTextbox.Size = new System.Drawing.Size(65, 26);
+            this.FirstTextbox.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FirstTextbox);
+            this.Controls.Add(this.SecondTextbox);
+            this.Controls.Add(this.FiftythTextbox);
+            this.Controls.Add(this.TwentythTextbox);
+            this.Controls.Add(this.FifthTextbox);
+            this.Controls.Add(this.TenthTextbox);
+            this.Controls.Add(this.HundredthTextbox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.v);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Gobutton);
+            this.Controls.Add(this.TotalmoneyTextbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TotalmoneyTextbox;
+        private System.Windows.Forms.Button Gobutton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label v;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox HundredthTextbox;
+        private System.Windows.Forms.TextBox TenthTextbox;
+        private System.Windows.Forms.TextBox FifthTextbox;
+        private System.Windows.Forms.TextBox TwentythTextbox;
+        private System.Windows.Forms.TextBox FiftythTextbox;
+        private System.Windows.Forms.TextBox SecondTextbox;
+        private System.Windows.Forms.TextBox FirstTextbox;
+    }
+}
+
---
+++ Moneyform/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moneyform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Gobutton_Click(object sender, EventArgs e)
+        {
+            int change = 0;
+            int prevChange = Int32.Parse(TotalmoneyTextbox.Text);
+            int boxNum = 0;
+
+            change = prevChange % 100;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 100;
+            HundredthTextbox.Text = "" + boxNum;
+
+            change = prevChange % 50;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 50;
+            FiftythTextbox.Text = "" + boxNum;
+
+            change = prevChange % 20;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 20;
+            TwentythTextbox.Text= "" + boxNum;
+
+            change = prevChange % 10;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 10;
+            TenthTextbox.Text= "" + boxNum;
+
+            change = prevChange % 5;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 5;
+            FifthTextbox.Text= "" + boxNum;
+
+            change = prevChange % 2;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 2;
+            SecondTextbox.Text= "" + boxNum;
+
+            change = prevChange % 1;
+            boxNum = prevChange - change;
+            prevChange = prevChange - boxNum;
+            boxNum = boxNum / 1;
+            FirstTextbox.Text= "" + boxNum;
+
+
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
